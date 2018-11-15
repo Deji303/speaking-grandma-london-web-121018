2cdb0b23f8552fe24596b3b6dcff185eb089a2fa
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-15 15:34:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,12 +6,6 @@ def speak_to_grandma (phrase)
   elsif phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
     puts "NO, NOT SINCE 1938!"
   else "I LOVE YOU TOO PUMPKIN!"
+
 end 
 
-# if dog == "hungry"
-#   puts "Refilling food bowl."
-# elsif dog == "thirsty"
-#   puts "Refilling water bowl."
-# else
-#   puts "Reading newspaper."
-# end
