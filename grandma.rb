39647be24ff1e582f4460251dac574b497a1bb4e
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-15 15:40:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,11 @@
-def speak_to_grandma (phrase)
-  if !(phrase == phrase.upcase)
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
-    puts "NO, NOT SINCE 1938!"
-  else "I LOVE YOU TOO PUMPKIN!"
+# Write a speak_to_grandma method.
 
+def speak_to_grandma (phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  else "HUH?! SPEAK UP, SONNY!"
 end 
 end 
+
